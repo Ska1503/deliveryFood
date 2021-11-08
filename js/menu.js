50a3bc88b793babbea9641eb740781de974ced5c
Vladyslav <you@example.com>
2021-11-09 00:16:28 +0300

    Dynamic rendering

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,44 @@
-const renderItems = (data) => {
-  data.forEach(elem => {
-    console.log(elem)
-  })
-  console.log(data)
+const cardsMenu = document.querySelector('.cards-menu')
+
+const chengeTitle = (restaurant) => {
+  const restaurantTitle = document.querySelector('.restaurant-title')
+  restaurantTitle.textContent = restaurant.name
 }
 
-fetch(`https://our-hull-329310-default-rtdb.firebaseio.com/db/partners.json`)
+const renderItems = (data) => {
+  data.forEach(({description,id,image,name,price}) => {
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    card.innerHTML = `
+      <img src="${image}" alt="${name}" class="card-image" />
+      <div class="card-text">
+        <div class="card-heading">
+          <h3 class="card-title card-title-reg">${name}</h3>
+        </div>
+        <div class="card-info">
+          <div class="ingredients">${description}
+          </div>
+        </div>
+        <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+            <span class="button-card-text">В корзину</span>
+            <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">${price} ₽</strong>
+        </div>
+      </div>
+    `
+    cardsMenu.append(card)
+  })
+}
+
+if (localStorage.getItem('restaurant')) {
+  const restaurant = JSON.parse(localStorage.getItem('restaurant'))
+
+  chengeTitle(restaurant)
+
+  fetch(`./db/${restaurant.produtcs}`)
   .then((response) => response.json())
   .then((data) => {
     renderItems(data)
@@ -13,3 +46,7 @@ fetch(`https://our-hull-329310-default-rtdb.firebaseio.com/db/partners.json`)
   .catch((error) => {
     console.log(error)
   })
+} else {
+  window.location.href =`/`
+}
+
